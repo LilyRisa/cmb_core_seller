4637b49e3cff3b54d98edb2eb17a366df704c31a
lilyrisa <bvminh101299@gmail.com>
2026-05-18 13:25:50 +0700

    feat(label): register FieldTypeRegistry singleton with 8 v0 field types

--- app/app/Modules/Fulfillment/FulfillmentServiceProvider.php
+++ app/app/Modules/Fulfillment/FulfillmentServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace CMBcoreSeller\Modules\Fulfillment;
 
+use CMBcoreSeller\Modules\Fulfillment\Services\LabelRendering\FieldTypeRegistry;
 use Illuminate\Support\ServiceProvider;
 
 /**
@@ -14,7 +15,23 @@ class FulfillmentServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        //
+        $this->app->singleton(FieldTypeRegistry::class, function ($app) {
+            $r = new FieldTypeRegistry();
+            foreach ([
+                \CMBcoreSeller\Modules\Fulfillment\Services\LabelRendering\Fields\QrField::class,
+                \CMBcoreSeller\Modules\Fulfillment\Services\LabelRendering\Fields\BarcodeField::class,
+                \CMBcoreSeller\Modules\Fulfillment\Services\LabelRendering\Fields\TextField::class,
+                \CMBcoreSeller\Modules\Fulfillment\Services\LabelRendering\Fields\ImageField::class,
+                \CMBcoreSeller\Modules\Fulfillment\Services\LabelRendering\Fields\DataField::class,
+                \CMBcoreSeller\Modules\Fulfillment\Services\LabelRendering\Fields\ItemsListField::class,
+                \CMBcoreSeller\Modules\Fulfillment\Services\LabelRendering\Fields\DividerField::class,
+                \CMBcoreSeller\Modules\Fulfillment\Services\LabelRendering\Fields\RectangleField::class,
+            ] as $cls) {
+                $r->register($app->make($cls));
+            }
+
+            return $r;
+        });
     }
 
     public function boot(): void
